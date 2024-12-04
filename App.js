24bd8cec6331429e0f7a96531edf4188fe7586f5
Giuseppe Orlandi <giuseppe.po@hotmail.com>
2024-12-04 09:52:11 -0300

    Criação do projeto e instalação dos pacotes do React Navigation

---
+++ App.js
@@ -0,0 +1,9 @@
+import { SafeAreaView, StatusBar } from "react-native"
+import AppRotas from "./src/rotas/AppRotas"
+
+export default function App() {
+    return <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar />
+        <AppRotas />
+    </SafeAreaView>
+}
